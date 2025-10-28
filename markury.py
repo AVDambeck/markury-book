5cd6db5367b795550b3b117a10f1cfc4debca411
Kathryn AV Dambeck <audiovisual@dambeck.org>
2025-10-28 00:42:23 -0400

    added flag for input dir and verbose

--- markury.py
+++ markury.py
@@ -9,3 +9,23 @@ combine the pdfs.
     index.html first, then the rest alphabetically.
 save the final pdf
 '''
+
+import argparse
+
+parser = argparse.ArgumentParser(description='book rendering tool.')
+
+parser.add_argument('-v', '--verbose', action='store_true', help='increase verbosity.')
+parser.add_argument('-i', '--input-dir', type=str, help='directory of input files. defaults to ../')
+
+args = parser.parse_args()
+
+if args.input_dir is None:
+    input_dir = '..'
+else:
+    input_dir = args.input_dir
+
+if args.verbose is not None:
+    pass
+    
+print(input_dir)
+

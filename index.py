b2d9ea18c1718b947dd2e2d7786ecbbacd37eb4d
Kathryn AV Dambeck <audiovisual@dambeck.org>
2025-11-05 14:36:47 -0500

    outlined indexing script

---
+++ index.py
@@ -0,0 +1,12 @@
+
+"""
+get the target dir.
+see if an index exists
+    1. create
+    2. append
+    3. overwrite
+look for everything that would get a link
+put it in the index.
+"""
+
+print("placeholder script")

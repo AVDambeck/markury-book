355a09b108d8b37df1c0289806c23e30ae3167d0
Kathryn AV Dambeck <audiovisual@dambeck.org>
2025-11-09 19:57:42 -0500

    started this- might back track

--- index.py
+++ index.py
@@ -9,4 +9,22 @@ look for everything that would get a link
 put it in the index.
 """
 
-print("placeholder script")
+target_dir = '/path/to/fir/'
+mode = 1
+
+def check_for_index(dir):
+    index_path = f'{dir}/index.md'
+    if os.path.exists(index_path):
+        if mode == 1:
+            print(f'{index_path} already exists.')
+            exit()
+        if mode == 3:
+            try:
+                os.remove(index_path)
+            except Exception as e:
+                print(f"An error occurred while deleting: {e}")
+                exit()
+    else:
+        return False
+
+
